a50e00c5d54ffd66717d7c6262ccaddb23ba2d38
Ian da Silva Correia <ianda.silvacorreia@outlook.com>
2023-10-29 22:08:53 -0300

    futher changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./src/App";
-import reportWebVitals from "./src/reportWebVitals";
-import { GamesProvider } from "./src/contexts/GamesContext";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { GamesProvider } from "./contexts/GamesContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
