a95a24aae2b1db85b308229abc38e0c7da6eb5a1
Ian da Silva Correia <ianda.silvacorreia@outlook.com>
2023-12-04 00:58:01 -0300

    Updated the API

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
-const options = {
-  method: "GET",
-  url: "http://localhost:3002/game",  // Ajuste o URL conforme necessário para a rota específica no seu backend
-};
+const url = "http://localhost:3002/";
 
 const getDataFromAPI = async () => {
+  const options = {
+    method: "GET",
+    url: `${url}/game`,
+  };
   try {
     const response = await axios.request(options);
     console.log(response.data);
@@ -15,4 +16,24 @@ const getDataFromAPI = async () => {
   }
 };
 
-export default getDataFromAPI;
+const postRegisterGame = async (obj) => {
+  try {
+    const response = await axios.post(`${url}/game`, { ...obj });
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const postLogin = async (obj) => {
+  try {
+    const response = await axios.post(`${url}/users/login`, { ...obj });
+    console.log(response.data);
+    return response.data.token;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export { getDataFromAPI, postRegisterGame, postLogin };
