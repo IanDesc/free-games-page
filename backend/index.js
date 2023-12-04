28f3ea0ef794310a77c69fb054f5596f1f4d5ed9
Kainã de Freitas Richalski <kaina-richalski@hotmail.com>
2023-12-04 00:05:42 -0300

    api local

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,17 @@
-const express = require("express")
-const path = require("path")
-require("dotenv").config()
+const express = require("express");
+const path = require("path");
+const cors = require("cors");  
+require("dotenv").config();
 
-const app = express()
-app.use(express.json())
-app.use(express.static(path.join(__dirname, 'public')))
+const app = express();
+app.use(cors());  
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/users", require("./routes/UserAPI"))
-app.use("/install", require("./routes/InstallAPI"))
-app.use("/game", require("./routes/GameAPI"))
-
+app.use("/users", require("./routes/UserAPI"));
+app.use("/install", require("./routes/InstallAPI"));
+app.use("/game", require("./routes/GameAPI"));
 
 app.listen(3002, () => {
-  console.log("Listening...")
-})
+  console.log("Listening...");
+});
---
+++ src/services/apilocal.js
@@ -0,0 +1,20 @@
+/*
+import axios from "axios";
+
+const options = {
+  method: "GET",
+  url: "http://localhost:3002/game",  // Ajuste o URL conforme necessário para a rota específica no seu backend
+};
+
+const getDataFromAPI = async () => {
+  try {
+    const response = await axios.request(options);
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export default getDataFromAPI;
+*/
